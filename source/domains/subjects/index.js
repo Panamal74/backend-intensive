e087cf48ad8b51d5cd9dd57943dbe022a06271a9
Panamal74 <pavlo.malakhovskyi@gmail.com>
2019-04-09 12:49:43 +0300

    befor hw-22

--- source/domains/subjects/index.js
+++ source/domains/subjects/index.js
@@ -1,7 +1,36 @@
 // Core
 import dg from 'debug';
 
+// Instruments
+import { Subjects } from '../../controllers';
+
 const debug = dg('router:subjects:subject');
+
+export const getSubjects = async (req, res) => {
+    debug(`${req.method} — ${req.originalUrl}`);
+
+    try {
+        const subjects = new Subjects();
+        const data = await subjects.find();
+
+        res.status(200).json({ data });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+export const createSubject = async (req, res) => {
+    debug(`${req.method} — ${req.originalUrl}`);
+
+    try {
+        const subjects = new Subjects(req.body);
+        const data = await subjects.create();
+
+        res.status(200).json({ data });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
 
 export const get = (req, res) => {
     debug(`${req.method} — ${req.originalUrl}`);
